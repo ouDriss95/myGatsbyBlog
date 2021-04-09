f17f7d6a91a53c9dbad114dcffc25f34aef7ade6
oudriss95 <unbreakabledriss@gmail.com>
2021-04-09 23:44:44 +0100

    Edit something

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,16 +10,8 @@ module.exports = {
     },
     social: [
       {
-        name: `github`,
-        url: `https://github.com/ouDriss95`,
-      },
-      {
         name: `instagram`,
         url: `https://www.instagram.com/doudmine/`,
-      },
-      {
-        name: `behance`,
-        url: `https://www.behance.net/unbreakabl16d0`,
       },
       {
         name: `youtube`,
@@ -29,13 +21,33 @@ module.exports = {
   },
   
   plugins: [
-    // {
-    //   resolve: 'gatsby-source-contentful',
-    //   options: {
-    //     spaceId: process.env.CONTENTFUL_SPACE_ID,
-    //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "GA-TRACKING_ID", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
     {
       resolve: "@narative/gatsby-theme-novela",
       options: {
